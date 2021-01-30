7ec75337cff3d71b417a0eb7e2ea308d3c9cf7ca
Alti-The-Potato <futurewaffles1@gmail.com>
2021-01-30 14:52:53 +0800

    Edited the program to test TalonMotor

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -24,18 +24,22 @@ void Robot::RobotInit() {
 	//init controllers
 	xbox = new frc::XboxController(0);
 
-	//Motor examples
-//	_sparkMotor = new frc::Spark(0);
-	_talonMotorL = new wml::TalonSrx(1);
-	_talonMotorR = new wml::TalonSrx(2);
-	_victorSpxMotorL = new wml::VictorSpx(8);
-	_victorSpxMotorR = new wml::VictorSpx(9);
+	_testMotor = new wml::TalonSrx(7);
 
-//	_sparkMotor->SetInverted(true);
-	_talonMotorL->SetInverted(false);
-	_talonMotorR->SetInverted(false);
-	_victorSpxMotorL->SetInverted(false);
-	_victorSpxMotorR->SetInverted(false);
+	_testMotor->SetInverted(false);
+
+// 	//Motor examples
+// //	_sparkMotor = new frc::Spark(0);
+// 	_talonMotorL = new wml::TalonSrx(1);
+// 	_talonMotorR = new wml::TalonSrx(2);
+// 	_victorSpxMotorL = new wml::VictorSpx(8);
+// 	_victorSpxMotorR = new wml::VictorSpx(9);
+
+// //	_sparkMotor->SetInverted(true);
+// 	_talonMotorL->SetInverted(false);
+// 	_talonMotorR->SetInverted(false);
+// 	_victorSpxMotorL->SetInverted(false);
+// 	_victorSpxMotorR->SetInverted(false);
 }
 
 void Robot::RobotPeriodic() {}
@@ -54,44 +58,18 @@ void Robot::TeleopPeriodic() {
 	currentTime = Timer::GetFPGATimestamp();
 	dt = currentTime - lastTimeStamp;
 
-	//motor examples
-//	sparkSpeed = xbox->GetY(hand::kLeftHand);
-//	_sparkMotor->Set(sparkSpeed);
-
-	// Make left side motors move based off left hand joystick
-	dbLeftSpeed = xbox->GetY(hand::kLeftHand);
-	if (abs(dbLeftSpeed) >= deadzone) {
-		_talonMotorL->Set(dbLeftSpeed);
-		_victorSpxMotorL->Set(dbLeftSpeed);
+	if (xbox->GetYButtonPressed()) {
+		_testMotor->Set(1);
 	} else {
-		_talonMotorL->Set(0);
-		_victorSpxMotorL->Set(0);
+		_testMotor->Set(0);
 	}
 
-	// Make right side motors move based off left hand joystick 
-	dbRightSpeed = xbox->GetY(hand::kRightHand);
-	if (abs(dbRightSpeed) >= deadzone) {
-		_talonMotorR->Set(dbRightSpeed);
-		_victorSpxMotorR->Set(dbRightSpeed);
+	if (xbox->GetAButtonPressed()) {
+		_testMotor->Set(-1);
 	} else {
-		_talonMotorR->Set(0);
-		_victorSpxMotorR->Set(0);
+		_testMotor->Set(0);
 	}
 
-	// ^ the equivilant using a conditional statement
-	//talonSpeed = xbox->GetTriggerAxis(hand::kRightHand) > deadzone ? xbox->GetTriggerAxis(hand::kRightHand) : 0; _talonMotor->Set(talonSpeed);
-
-//	if(xbox->GetXButton()) {
-//		_solenoid.SetTarget(wml::actuators::BinaryActuatorState::kForward);
-//	} else {
-//		_solenoid.SetTarget(wml::actuators::BinaryActuatorState::kReverse);
-//	}
-
-	_compressor.Update(dt);
-	_solenoid.Update(dt);
-
-	if (_solenoid.IsDone()) _solenoid.Stop();
-	lastTimeStamp = currentTime;
 }
 
 // Test Logic
--- 4788/src/main/include/Robot.h
+++ 4788/src/main/include/Robot.h
@@ -55,15 +55,17 @@ class Robot : public frc::TimedRobot {
 
  private:
 	//motors
-	frc::Spark *_sparkMotor;
+	// frc::Spark *_sparkMotor;
 
 
-	wml::TalonSrx *_talonMotorL;
-	wml::TalonSrx *_talonMotorR;
-	wml::VictorSpx *_victorSpxMotorL;
-	wml::VictorSpx *_victorSpxMotorR;
+	// wml::TalonSrx *_talonMotorL;
+	// wml::TalonSrx *_talonMotorR;
+	// wml::VictorSpx *_victorSpxMotorL;
+	// wml::VictorSpx *_victorSpxMotorR;
+
+	 wml::TalonSrx *_testMotor;
 
 	//pneumatics stuff
-	wml::actuators::Compressor _compressor{};
-	wml::actuators::DoubleSolenoid _solenoid{0, 1, 0.2};
+	// wml::actuators::Compressor _compressor{};
+	// wml::actuators::DoubleSolenoid _solenoid{0, 1, 0.2};
 };
